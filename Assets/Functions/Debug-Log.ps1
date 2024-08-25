e444c9552a78cec062c9064651a7bace143c8465
SparksSkywere <Sparks-Skywere@outlook.com>
2024-08-25 18:06:21 +0100

    deleted:    main.ps1

--- Assets/Functions/Debug-Log.ps1
+++
@@ -1,9 +0,0 @@
-# Wrapper function to handle conditional logging
-function Debug-Log {
-    param (
-        [string]$Message
-    )
-    if ($global:DebugLoggingEnabled) {
-        Write-Host $Message
-    }
-}
